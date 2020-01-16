52207207b9658d9b553d8356fa52cf74f0fee3a9
n-k-s <49578275+n-k-s@users.noreply.github.com>
2020-01-16 11:36:59 -0600

    edits

--- Homework1/stat.py
+++ Homework1/stat.py
@@ -4,14 +4,19 @@ def avg(X):
         tempAvg += X[i]
     return 1.0 / len(X) * tempAvg
 
-myNum = [1,2,3]
+
+myNum = [1, 2, 3]
 print(avg(myNum))
+
 
 def l_sqr(X):
     b = []
     b = [i ** 2 for i in X]
     return b
+
+
 print(l_sqr(myNum))
+
 
 def var(X):
     return 1
