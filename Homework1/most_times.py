1b029cbad72c056cc20d39ea27258eec00239c62
n-k-s <49578275+n-k-s@users.noreply.github.com>
2020-01-16 12:00:45 -0600

    edits

--- Homework1/most_times.py
+++ Homework1/most_times.py
@@ -4,7 +4,6 @@ from collections import Counter
 mylist = [i for i in argv]
 if len(mylist) > 1:
     mylist.pop(0)
-print(Counter(mylist))
 dict = (Counter(mylist).most_common(1))
 print(dict[0][0])
 
