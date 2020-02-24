a754ec0ce81dcf88ccd3c19e582df951d65c667b
n-k-s <49578275+n-k-s@users.noreply.github.com>
2020-02-24 13:03:17 -0600

    working

--- Homework4/n-trigrams.py
+++ Homework4/n-trigrams.py
@@ -12,28 +12,44 @@ from sys import argv
 def ngrams(text, n):
     return [' '.join(text[i:i + n]) for i in range(len(text) - n + 1)]
 
-
-def purgeEmpty(list):
+def cleanNoise(theList):
+    a = []
+    for i in theList:
+        i = i.replace("'s", "").replace("*","").replace("~","")
+        a.append(i)
+    return a
+def emptyListElement(theList):
+    for i in theList:
+        if len(i) == 0:
+            theList.remove(i)
+    return theList
+def listOfLists(list):
+    # takes a list, splits on spaces, and makes a list of lists
     a = []
     for i in list:
-        if len(i) > 0:
-            a.append(i)
+        b = i.split(" ")
+        a.append(b)
     return a
+def splitToSentences(text):
+
+def clean(text):
+    list = splitToSentences(text)
+    list = cleanNoise(list)
+    list = emptyListElement(list)
+    list = listOfLists(list)
+    return list
 
 
 # argv 0 program name, 1 input file, 2 output file
 
 importFile = open(argv[1])
 exportFile = argv[2]
-a = ""
-for i in importFile:
-    a += i
-b = re.split("[.][ ]|[\r\n]+", a)
+text = importFile.read()
+sentences = clean(re.split("[.!?][ ]|[\r\n]+", text))
 
-b = purgeEmpty(b)
-pprint(b)
-print(len(b))
-print()
+
+pprint(sentences)
+
 # print(b[-1])
 # export = open(exportFile, "w")
 #
