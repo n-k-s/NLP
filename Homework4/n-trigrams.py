6ad26d42cc1908aed92deaa5aeb2c52ba18a6687
n-k-s <49578275+n-k-s@users.noreply.github.com>
2020-02-25 15:07:44 -0600

    unigrams work

--- Homework4/n-trigrams.py
+++ Homework4/n-trigrams.py
@@ -10,27 +10,37 @@ from sys import argv
 
 
 def ngrams(text, n):
-    if (n == 1):
-        return [' '.join(text[i:i + n]) for i in range(len(text) - n + 1)]
-    else:
-        text = gramFormatter(text, n)
-        return [' '.join(text[i:i + n]) for i in range(len(text) - n + 1)]
-def gramFormatter(list, n):
+    return [' '.join(text[i:i + n]) for i in range(len(text) - n + 1)]
+
+
+def endSentence(list):
+    newList = []
     for i in list:
-        for i in range(n):
-            i.insert(0 ,"*")
-    return i
+        i.append("STOP")
+        newList.append(i)
+    return newList
+
+def startSentence(list):
+    for i in list:
+        i.insert(0, "*")
+    return list
+
+
 def cleanNoise(theList):
     a = []
     for i in theList:
-        i = i.replace("'s", "").replace("*","").replace("~","")
+        i = i.replace("'s", "").replace("*", "").replace("~", "").replace(":", "").replace(",","").replace("_","").replace("--","")
         a.append(i)
     return a
+
+
 def emptyListElement(theList):
     for i in theList:
         if len(i) == 0:
             theList.remove(i)
     return theList
+
+
 def listOfLists(list):
     # takes a list, splits on spaces, and makes a list of lists
     a = []
@@ -38,8 +48,12 @@ def listOfLists(list):
         b = i.split(" ")
         a.append(b)
     return a
+
+
 def splitToSentences(text):
     return re.split("[.!?][ ]|[\r\n]+", text)
+
+
 def clean(text):
     # takes the text as a long string, converts to lists divided by sentences, divided into words
     list = splitToSentences(text)
@@ -53,16 +67,32 @@ def clean(text):
 
 # argv 0 program name, 1 input file, 2 output file
 
-importFile = open(argv[1])
+importFile = open(argv[1], encoding="utf8")
 exportFile = argv[2]
 text = importFile.read()
 sentences = clean(text)
 
-print(sentences)
+# adds stop to the end
+endSentence(sentences)
+allgrams = ""
+# unigrams
+for i in sentences:
+    for j in i:
+        allgrams += j + " "
+a = Counter(allgrams.split())
+print(a)
+export = open(exportFile, "w", encoding="utf8")
+for i in a.most_common():
+    print(i[1])
+    export.write(i[0] + "\t" + str(i[1]) + "\n")
 
-# print(b[-1])
-# export = open(exportFile, "w")
-# export.write("")
 
-# export.close()
+
+# bigrams = ngrams(bigram, 2)
+# trigrams = ngrams(trigram, 3)
+
+
+
+
+export.close()
 importFile.close()
