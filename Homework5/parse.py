a3d58f686da1f76de236fa331e3a8d8ccc84fcc0
n-k-s <49578275+n-k-s@users.noreply.github.com>
2020-03-27 15:09:05 -0500

    starting viterbi

--- Homework5/parse.py
+++ Homework5/parse.py
@@ -48,15 +48,18 @@ def emissions_dict_matcher(word, emissions_dictionary):
 
 def sentence_formatting(list_of_sentences):
     temp = list_of_sentences
-    temp = remove_new_line_and_tabs(temp)
-    # temp = add_stop_to_end_of_sentence(list_of_sentences)
-    # temp = add_star_to_end_of_sentence(temp)
+    # temp = remove_new_line_and_tabs(temp)
+    temp = add_stop_to_end_of_sentence(list_of_sentences)
+    temp = add_star_to_start_of_sentence(temp)
+    temp = add_star_to_start_of_sentence(temp)
     return temp
 
 
 def remove_new_line_and_tabs(list_of_sentences):
     temp = []
     for i in list_of_sentences:
+        if i[0] == '\t' and i[-1] == '\n':
+            i = i[1:-1]
         if i[-1] == '\n':
             i = i[:-1]
         if i[0] == '\t':
@@ -73,7 +76,7 @@ def add_stop_to_end_of_sentence(list_of_sentences):
     return temp
 
 
-def add_star_to_end_of_sentence(list_of_sentences):
+def add_star_to_start_of_sentence(list_of_sentences):
     temp = []
     for i in list_of_sentences:
         i = "*/* " + i
@@ -81,14 +84,31 @@ def add_star_to_end_of_sentence(list_of_sentences):
     return temp
 
 
+def transmissions(list_of_tags, given1, given2, trigrams_dict):
+    transmission_probabilities = []
+    running_total = 0
+    for i in list_of_tags:
+        dictionary_match = given1 + " " + given2 + " " + i
+        transmission_probabilities.append([dictionary_match, trigrams_dict[dictionary_match]])
+        running_total += int(trigrams_dict[dictionary_match])
+    for n in transmission_probabilities:
+        n[1] = int(n[1]) / running_total
+    return transmission_probabilities
+
+
 def veterbi():
+    # pi table 0=k_index 1=u 2=v 3=pi 4=bp
+    pi_table = [[0],["*"],["*"],[""]]
+
     return -1
 
 
 # argv 0 program name, 1 training file, 2 test file
 # training_file = open(argv[1]).read()
 # test_file = open(argv[2])
+# TODO change this back to argv and not training text
 training_file = open("ca_train.txt").read()
+train_file = open("ca_train.txt")
 test_file = open("ca_test.txt")
 
 training_split = training_file.split()
@@ -99,13 +119,25 @@ non_terminals = Counter(split_tags(training_split))
 two_non_terminals = Counter(ngrams(split_tags(training_split), 2))
 three_non_terminals = Counter(ngrams(split_tags(training_split), 3))
 
-test_file_sentences = test_corpus_new_line_cleanse(test_file.readlines())
+### my verterbi ###
 
-# print(test_file_sentences[1])
-test_file_sentences = sentence_formatting(test_file_sentences)
+# each sentence is a list TEST FILE
+test_file_sentences_formatted = sentence_formatting(test_corpus_new_line_cleanse(test_file.readlines()))
+# each sentence is a list TRAIN FILE
+train_file_sentences_formatted = sentence_formatting(test_corpus_new_line_cleanse(train_file.readlines()))
 
-print(test_file_sentences)
+# creating the transmission_trigram_dictionary, tags in order for the transmissions
+pre_ttd = []
+for i in train_file_sentences_formatted:
+    ttd = ngrams(split_tags(i.split()), 3)
+    for j in ttd:
+        pre_ttd.append(j)
 
-# print(emissions_dict_matcher("left", emissions))
+transmissions_trigram_dictionary = Counter(pre_ttd)
+
+print(emissions_dict_matcher("left", emissions))
+print(transmissions(["nn", "jj", "pps"], "*", "*", transmissions_trigram_dictionary))
+
+pprint(test_file_sentences_formatted)
 
 #### test files ####
